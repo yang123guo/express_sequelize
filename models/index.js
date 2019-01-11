5e0006167cbb3784b5bf6e538866c5d63a19a960
yangguoqiang01 <wochange@163.com>
2019-01-11 17:36:50 +0800

    :bugs: dd

--- models/index.js
+++ models/index.js
@@ -1,36 +1,36 @@
 'use strict';
 
-var fs        = require('fs');
-var path      = require('path');
+var fs = require('fs');
+var path = require('path');
 var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.js')[env];
-var db        = {};
+var basename = path.basename(__filename);
+var env = process.env.NODE_ENV || 'development';
+var config = require(__dirname + '/../config/config.js')[env];
+var db = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (config.use_env_variable) { // 如果config的某项有use_env_variable键
+    var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else { // 没有use_env_variable键
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 
 // 将返回一个包含“指定目录下所有文件名称”对象组成的数组  同步读取
 fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    // 返回除了basename的文件对象 数组
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model; // 给db对象拓展 key为name  value为model对象
-  });
+    .readdirSync(__dirname)
+    .filter(file => {
+        // 返回除了basename的文件对象 数组
+        return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+    })
+    .forEach(file => {
+        var model = sequelize['import'](path.join(__dirname, file));
+        db[model.name] = model; // 给db对象拓展 key为name  value为model对象
+    });
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) { // 如果对象db下有associate 属性，那么执行associate()连接功能
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) { // 如果对象db下有associate 属性，那么执行associate()连接功能
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
-var models  = require('../models');
+var models = require('../models');
 var express = require('express');
-var router  = express.Router();
+var router = express.Router();
 
 
 // index模块的处理逻辑
-router.get('/', function(req, res) {
-  models.User.findAll({
-    include: [ models.Task ]
-  }).then(function(users) {
-    res.render('index', {
-      title: 'Sequelize: Express Example',
-      users: users
+router.get('/', function (req, res) {
+    models.User.findAll({
+        include: [models.Task]
+    }).then(function (users) {
+        res.render('index', {
+            title: 'Sequelize: Express Example',
+            users: users
+        });
     });
-  });
 });
 
 module.exports = router;
